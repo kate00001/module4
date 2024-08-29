8e78887f50acfe84c6472fbe0d898ac6c3d3b3d1
Kate <166659076+kate00001@users.noreply.github.com>
2024-08-30 00:53:35 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fake
+from true_math import divide as true
+
+fake(6,3)
+fake(6,0)
+true(12,4)
+true(12,0)
---
+++ true_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        from math import inf
+        print(inf)
+    else: print(first / second)
